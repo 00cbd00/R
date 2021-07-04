13542ef5d2c809bc234a78dbec4572576288e15c
00cbd00 <86519668+00cbd00@users.noreply.github.com>
2021-07-04 19:51:48 +0800

    Update 06.最新版KEGG解析.R

--- biotrainee_relevant/Live_activities_2017/06.最新版KEGG解析.R
+++ biotrainee_relevant/Live_activities_2017/06.最新版KEGG解析.R
@@ -7,7 +7,7 @@ library(stringr)
 
 
 # hsa00001.keg
-# https://www.genome.jp/kegg-bin/get_htext?hsa00001+3101
+# https://www.kegg.jp/brite/hsa00001.keg
 #################################################################################################
 # 数据格式不是很规律，不好读取，直接用 readLines 函数
 con = file("hsa00001.keg","r")
@@ -20,15 +20,17 @@ close(con)
 # 观察数据结构
 temp = as.data.frame(line[1:1000])
 #   !     #     %     +     A     B     C     D 
-#   2    11     1     1     8   112   534 50197 
+#   2    11     1     1     8   112   534 50200 
 table(substr(line,1,1))
-#  5  3176  4799  9682 12326 20533 33647 50250
+#  5  3176  4799  9682 12326 20533 33647 50252
 which(substr(line,1,1)=="A")
 # [1] "A09100 Metabolism"                           "A09120 Genetic Information Processing"      
 # [3] "A09130 Environmental Information Processing" "A09140 Cellular Processes"                  
 # [5] "A09150 Organismal Systems"                   "A09160 Human Diseases"                      
 # [7] "A09180 Brite Hierarchies"                    "A09190 Not Included in Pathway or Brite" 
 line[which(substr(line,1,1)=="A")]
+# 只提取前6大类A
+line = line[1:(which(substr(line,1,1)=="A")[7]-1)]
 
 # 提取C通路和D基因
 kegg_gene = line[which(substr(line,1,1)%in%c("C","D"))]
@@ -40,8 +42,13 @@ temp = kegg_gene[ID]
 head(temp)
 # 将每个C通路扩大至与对应的D基因数相同
 kegg_gene_2 = rep(temp,c(ID[2:length(ID)]-1,length(kegg_gene))-ID)
+# 提取通路编号
+temp = str_split(kegg_gene_2,"[:\\]]",simplify = T)
+# 部分通路没有 hsa***** 字样的编号,再次提取指定编号
+ID = which(temp[,2]=="")
+temp[ID,2] = str_split(temp[ID,1]," ",simplify = T)[,5]
 # 提取C编号和对应的通路名
-kegg_gene_2 = cbind(substr(kegg_gene_2,6,10),substr(kegg_gene_2,12,99999))
+kegg_gene_2 = cbind(temp[,2],substr(kegg_gene_2,12,99999))
 # 清除通路名中多余的通路编号
 kegg_gene_2[,2] = str_split(kegg_gene_2[,2]," \\[",simplify = T)[,1]
 
@@ -57,10 +64,11 @@ temp = str_split(temp,";",simplify = T)[,1]
 temp = str_split(temp,"\t",simplify = T)[,1]
 temp = gsub("^[0-9]* ","",temp)
 kegg_gene_2 = cbind(kegg_gene_2,temp)
-
+# 8104
+length(unique(kegg_gene_2[,3]))
 # 整合
 kegg_extract = data.frame(gene_id = as.numeric(kegg_gene_2[,3]),
-                          pathway_id = as.numeric(kegg_gene_2[,1]),
+                          pathway_id = kegg_gene_2[,1],
                           gene_name = kegg_gene_2[,4],
                           pathway_name = kegg_gene_2[,2])
 #################################################################################################
@@ -75,4 +83,7 @@ table(temp==kegg_extract)
 # kegg_extract = temp
 setwd("F:/code/biotrainee2017/data")
 #################################################################################################
+
+
+
 
